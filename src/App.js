9fac441dca7e8351616046184bce8ecb73c47e81
masoume <masoumevsl198@gmail.com>
2020-04-16 19:03:55 +0430

    add
    3 const

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 
 const App = () => {
 
-  const APP_ID = '598f79df';
+  const APP_ID = "598f79df";
+  const APP_KEY = "6b084ce5a194500f083460330ea21fa1";
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`}
+
   return (
     <div className="App">
       <h1>Hello React</h1>
