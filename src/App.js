22faf909b7c30e635c8a9b2148c7774eb768da1e
masoume <masoumevsl198@gmail.com>
2020-04-16 23:58:18 +0430

    edit
    code

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ const App = () => {
   const APP_ID = "598f79df";
   const APP_KEY = "6b084ce5a194500f083460330ea21fa1";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`}
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">search</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 export default App;
