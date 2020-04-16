548c19b80639d536d4eb9ad07de31b2ffeb3350c
masoume <masoumevsl198@gmail.com>
2020-04-16 18:50:07 +0430

    code
    App_ID

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
 
+const App = () => {
 
+  const APP_ID = '598f79df';
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+    </div>
+  )
+}
 
 export default App;
