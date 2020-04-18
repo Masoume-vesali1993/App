429c32b9139602f4e1c4581b807b3198e8bcac06
masoume <masoumevsl198@gmail.com>
2020-04-18 17:03:12 +0430

    counter
    add button

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ const App = () => {
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button onClick={() => setCounter(counter + 1)} className="search-button" type="submit">
-          {counter}
+        <button className="search-button" type="submit">
         </button>
       </form>
+      <h1 onClick = {() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 };
