5649a186085395b7361e34ce2c8beaccd0c753bf
masoume <masoumevsl198@gmail.com>
2020-04-18 17:53:05 +0430

    edit
    code

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ const App = () => {
   const APP_KEY = "6b084ce5a194500f083460330ea21fa1";
 
 
-    useEffect(() => {
+    useEffect( () => {
       getRecipes();
     }, []);
 
 
     const getRecipes = async () => {
-      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const response = await fetch(
+        `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
       const data = await response.json();
       console.log(data);
 
