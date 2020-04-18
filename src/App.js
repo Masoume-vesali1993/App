d0da60bd5518a97b05af9193c51fb7899ffd94f4
masoume <masoumevsl198@gmail.com>
2020-04-18 17:30:28 +0430

    add
    object

--- src/App.js
+++ src/App.js
@@ -8,15 +8,16 @@ const App = () => {
 
 
     useEffect(() => {
-      
+      getRecipes();
     }, []);
 
 
     const getRecipes = async () => {
       const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-      const data = response.json();
+      const data = await response.json();
+      console.log(data);
 
-    }
+    };
 
   return (
     <div className="App">
