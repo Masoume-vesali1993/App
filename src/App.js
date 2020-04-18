3d74b2011ddb7570a1b7b7792720c565069aafae
masoume <masoumevsl198@gmail.com>
2020-04-18 17:05:48 +0430

    Edit
    Button

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ const App = () => {
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">
-        </button>
+        <button className="search-button" type="submit"> search </button>
       </form>
       <h1 onClick = {() => setCounter(counter + 1)}>{counter}</h1>
     </div>
