ea4094d286519aca34cef3b6f2d59afef14b1011
masoume <masoumevsl198@gmail.com>
2020-04-18 16:57:28 +0430

    add
    Button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -8,11 +8,20 @@ const App = () => {
 
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
+  const [counter, setCounter] = useState(0);
+
+    useEffect(() =>{
+      console.log('Effect has been run');
+    });
+
+
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button className="search-button" type="submit">search</button>
+        <button onClick={() => setCounter(counter + 1)} className="search-button" type="submit">
+          {counter}
+        </button>
       </form>
     </div>
   );
